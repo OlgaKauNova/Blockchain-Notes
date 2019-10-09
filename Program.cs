77c4b09d6b5fe1271d88a89a767ae27c16cb8b42
Evgenii Kucheriavyi <e.kucheriavii@gmail.com>
2019-10-09 14:30:20 +0300

    Added some colors

--- Program.cs
+++ Program.cs
@@ -18,7 +18,7 @@ namespace blockchain
 		{
 			List<Note> notes = new List<Note>();
 
-			notes.Add(new Note("start  "));
+			notes.Add(new Note("start First note"));
 
 
 			string command = "";
@@ -27,9 +27,20 @@ namespace blockchain
 
 			while(com != Command.Exit)
 			{
+				Console.Clear();
+
+				Console.ForegroundColor = ConsoleColor.Green;
 				Console.WriteLine("Actions: ");
+
+				Console.ForegroundColor = ConsoleColor.White;
 				Console.WriteLine("0 - Show notes \n1 - Show full notes \n2 - Add note \n3 - Validate notes \n4 - Exit \n");
+
+				Console.ForegroundColor = ConsoleColor.Green;
+				Console.WriteLine("Your choise: ");
+
+				Console.ForegroundColor = ConsoleColor.White;
 				command = Console.ReadLine();
+				Console.WriteLine("\n");
 
 				try
 				{
@@ -52,7 +63,10 @@ namespace blockchain
 						break;
 
 					case Command.Add:
+						Console.ForegroundColor = ConsoleColor.Green;
 						Console.WriteLine("Enter new note text: ");
+
+						Console.ForegroundColor = ConsoleColor.White;
 						string text = Console.ReadLine();
 
 						AddNote(text, notes);
@@ -63,9 +77,13 @@ namespace blockchain
 						break;
 
 					case Command.Exit:
+						Console.ForegroundColor = ConsoleColor.Blue;
 						Console.WriteLine("Good Bye!");
+						Console.ForegroundColor = ConsoleColor.White;
 						break;
 				}
+
+				Console.ReadKey();
 			}
 
 			
@@ -77,22 +95,28 @@ namespace blockchain
 			{
 				if(i == 0)
 				{
+					Console.ForegroundColor = ConsoleColor.Green;
 					Console.WriteLine("0 - Valid");
 				}
 				else
 				{
+					Console.ForegroundColor = ConsoleColor.White;
+					Console.WriteLine($"Previous hash: {notes[i].PreviousHash} | Current hash: {notes[i - 1].HashString}");
+
 					if(notes[i].PreviousHash == notes[i - 1].HashString)
 					{
+						Console.ForegroundColor = ConsoleColor.Green;
 						Console.WriteLine(i + " - Valid");
 					}
 					else
 					{
+						Console.ForegroundColor = ConsoleColor.Red;
 						Console.WriteLine((i - 1) + " - Changed");
 						break;
 					}
 				}
 			}
-			Console.WriteLine("");
+			Console.WriteLine("\n");
 		}
 
 		static void AddNote(string text, List<Note> notes)
@@ -102,7 +126,10 @@ namespace blockchain
 
 		static void WriteAllNotes(List<Note> notes)
 		{
+			Console.ForegroundColor = ConsoleColor.Green;
 			Console.WriteLine("Your notes: ");
+
+			Console.ForegroundColor = ConsoleColor.White;
 			for(int i = 0; i < notes.Count; i++)
 			{
 				WriteNote(notes[i]);
@@ -113,7 +140,10 @@ namespace blockchain
 
 		static void WriteFullNotes(List<Note> notes)
 		{
+			Console.ForegroundColor = ConsoleColor.Green;
 			Console.WriteLine("Your notes: ");
+
+			Console.ForegroundColor = ConsoleColor.White;
 			for(int i = 0; i < notes.Count; i++)
 			{
 				WriteNoteFull(notes[i]);
